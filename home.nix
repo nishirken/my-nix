ef506e9a6ee2fda6fd07a1417e255981e0b4e493
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2021-12-02 14:43:14 +0300

    vscode direnv

--- home.nix
+++ home.nix
@@ -109,6 +109,12 @@ args@{ config, pkgs, ... }:
           version = "3.4.0";
           sha256 = "fe989d59bc93fa1807ec6b2554060ad6ee51266312bccaa3ea72aafe65a96729";
         }
+        {
+          name = "vscode-direnv";
+          publisher = "cab404";
+          version = "1.0.0";
+          sha256 = "fa72c7f93f6fe93402a8a670e873cdfd97af43ae45566d92028d95f5179c3376";
+        }
       ];
     };
   };
@@ -155,6 +161,7 @@ args@{ config, pkgs, ... }:
     (makeAutostartItem { name = "libinput-gestures"; package = libinput-gestures; })
     tmux
     cabal2nix
+    nodejs
   ];
 
   nixpkgs.config.allowUnfree = true;
