78f1488ccaf23b237df6052ec03b6cdea0928e64
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2021-12-24 15:57:32 +0300

    vim update

--- vim.nix
+++ vim.nix
@@ -7,6 +7,44 @@
       vim-nix
       haskell-vim
       coc-nvim
+      vim-one
     ];
+    vimAlias = true;
+    extraConfig = ''
+      nnoremap <silent> h :call CocActionAsync('doHover')<cr>
+
+      colorscheme one
+      let g:one_allow_italics = 1
+
+      let hr = (strftime('%H'))
+      if hr >= 20 || hr <= 9
+        set background=dark
+      endif
+
+      if exists('+termguicolors')
+        let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
+        let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
+        set termguicolors
+      endif
+    '';
+    coc = {
+      enable = true;
+      settings = {
+        languageserver = {
+          haskell = {
+            command = "haskell-language-server";
+            args = [ "--lsp" "--project-ghc-version" ];
+            rootPatterns = [
+              "*.cabal"
+              "stack.yaml"
+              "cabal.project"
+              "package.yaml"
+              "hie.yaml"
+            ];
+            filetypes = [ "haskell" "lhaskell" ];
+          };
+        };
+      };
+    };
   };
 }
