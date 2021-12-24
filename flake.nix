fba9ff4065d20cf3c158831f63bb1740f5823d49
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2021-12-24 15:56:34 +0300

    21.11 update

--- flake.nix
+++ flake.nix
@@ -1,12 +1,12 @@
 {
-  inputs.nixpkgs.url = "github:nishirken/nixpkgs/nixos-21.05-patched";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
   inputs.nixpkgs_unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.home-manager.url = "github:nix-community/home-manager/release-21.05";
+  inputs.home-manager.url = "github:nix-community/home-manager/release-21.11";
 
   outputs = { self, nixpkgs, nixpkgs_unstable, home-manager, ... }:
 
   let
-    patched = (import nixpkgs { system = "x86_64-linux"; });
+    stable = (import nixpkgs { system = "x86_64-linux"; });
     unstable = (import nixpkgs_unstable { system = "x86_64-linux"; }); in {
 
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
@@ -16,11 +16,11 @@
         ./hardware-configuration.nix
         ./environment.nix
 
-	      home-manager.nixosModules.home-manager
+	home-manager.nixosModules.home-manager
 
         ({ pkgs, ... }: {
           nix.extraOptions = "experimental-features = nix-command flakes";
-          nix.package = unstable.nix;
+          nix.package = stable.nix_2_4;
           nix.registry.nixpkgs.flake = nixpkgs;
           home-manager.useUserPackages = true;
           home-manager.useGlobalPkgs = true;
@@ -36,12 +36,13 @@
       system = "x86_64-linux";
       homeDirectory = "/home/nish";
       username = "nish";
-      stateVersion = "21.05";
-      pkgs = patched;
+      stateVersion = "21.11";
+      pkgs = stable;
       configuration.imports = [
         ./home.nix
         ./emacs.nix
-	./vim.nix
+        ./vim.nix
+        ./code.nix
       ];
     };
   };
