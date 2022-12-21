350f278a339a9aed719af9ec1a69f19218ecf432
Dmitriy Skurikhin <dmitrii.sk@gmail.com>
2022-12-21 17:33:49 +0400

    add ncurses as steam dependency for victoria3

--- environment.nix
+++ environment.nix
@@ -1,24 +1,32 @@
-  { pkgs, ... }:
+{ pkgs, config, ... }:
 
-  {
-    environment = with pkgs; {
-      variables = {
-        BROWSER = "firefox";
-        EDITOR = "nvim";
-      };
-      gnome.excludePackages = [ xterm ];
-      shells = [ zsh ];
+{
+  environment = with pkgs; {
+    variables = {
+      BROWSER = "firefox";
+      EDITOR = "nvim";
+    };
+    gnome.excludePackages = [ xterm ];
+    shells = [ zsh ];
 
-      systemPackages = [
-        xorg.xbacklight
-        parted
-        pciutils
-        lshw
-        xclip
-      ];
+    systemPackages = [
+      xorg.xbacklight
+      parted
+      pciutils
+      lshw
+      xclip
+    ];
   };
 
-    programs.steam.enable = true;
-    programs.light.enable = true;
-    services.blueman.enable = true;
-  }
+  programs.steam = {
+    enable = true;
+    package = pkgs.steam.override {
+      extraLibraries = pkgs: with config.hardware.opengl;
+        if pkgs.hostPlatform.is64bit
+        then [ package pkgs.ncurses ] ++ extraPackages
+        else [ package32 ] ++ extraPackages32;
+      };
+  };
+  programs.light.enable = true;
+  services.blueman.enable = true;
+}
