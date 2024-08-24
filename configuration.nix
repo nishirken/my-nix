53d1b23faed95fa1a204891c62dec23b3c311310
Dmitriy Skurikhin <dmitrii.sk@gmail.com>
2024-08-24 18:43:22 +0400

    tlp

--- configuration.nix
+++ configuration.nix
@@ -39,6 +39,22 @@
       };
     };
     accounts-daemon.enable = true;
+    power-profiles-daemon.enable = false;
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "conservative";
+
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+        #Optional helps save long term battery health
+        START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
+        STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+
+      };
+    };
   };
 
   users = {
